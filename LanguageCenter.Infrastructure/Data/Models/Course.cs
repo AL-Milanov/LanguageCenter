4e5229e4db616c1d7e700ec041acd85224b1bafc
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-23 23:19:59 +0200

    added course entity

---
+++ LanguageCenter.Infrastructure/Data/Models/Course.cs
@@ -0,0 +1,49 @@
+﻿using LanguageCenter.Core.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanguageCenter.Core.Data.Models
+{
+    public class Course
+    {
+        [Key]
+        [StringLength(Constraints.GuidIdLenght)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(25)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string Level { get; set; }
+
+        public short DurationInMonths { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [StringLength(Constraints.GuidIdLenght)]
+        public string TeacherId { get; set; }
+
+        [ForeignKey(nameof(TeacherId))]
+        public virtual Teacher Teacher { get; set; }
+
+        [Required]
+        [StringLength(Constraints.GuidIdLenght)]
+        public string LanguageId { get; set; }
+
+        [ForeignKey(nameof(LanguageId))]
+        public virtual Language Language { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+        public Course()
+        {
+            EndDate = StartDate.AddMonths(DurationInMonths);
+        }
+
+    }
+}
