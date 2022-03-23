c2db8c9ac6ff3a161ae2a173a839679905443fd3
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-23 23:02:08 +0200

    changed fields names to start with capital letter

--- LanguageCenter.Infrastructure/Data/Common/Constraints.cs
+++ LanguageCenter.Infrastructure/Data/Common/Constraints.cs
@@ -2,14 +2,15 @@
 {
     public static class Constraints
     {
+        public const int GuidIdLenght = 36;
 
         public static class ApplicationUser
         {
-            public const int firstNameMinLength = 2;
-            public const int firstNameMaxLength = 55;
-
-            public const int lastNameMinLength = 2;
-            public const int lastNameMaxLength = 70;
+            public const int FirstNameMinLength = 2;
+            public const int FirstNameMaxLength = 55;
+                             
+            public const int LastNameMinLength = 2;
+            public const int LastNameMaxLength = 70;
         }
     }
 }
