8e14a3d9dcc7d0e8852c7446cc678dba10af5c96
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-23 22:31:20 +0200

    added application user class and modified context to work with it

---
+++ LanguageCenter.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using LanguageCenter.Core.Data.Common;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanguageCenter.Core.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(Constraints.ApplicationUser.firstNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(Constraints.ApplicationUser.lastNameMaxLength)]
+        public string LastName { get; set; }
+    }
+}
