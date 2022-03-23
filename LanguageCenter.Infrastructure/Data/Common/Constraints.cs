f231924e419224cc744e2b0f9f727f322a2eecee
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-23 22:30:47 +0200

    class that contains constraints for db entities

---
+++ LanguageCenter.Infrastructure/Data/Common/Constraints.cs
@@ -0,0 +1,15 @@
+﻿namespace LanguageCenter.Core.Data.Common
+{
+    public static class Constraints
+    {
+
+        public static class ApplicationUser
+        {
+            public const int firstNameMinLength = 2;
+            public const int firstNameMaxLength = 55;
+
+            public const int lastNameMinLength = 2;
+            public const int lastNameMaxLength = 70;
+        }
+    }
+}
