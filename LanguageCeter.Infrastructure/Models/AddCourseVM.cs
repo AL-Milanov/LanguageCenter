11c700314efd6347882da83364303d0f91f2cde6
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-30 13:35:20 +0300

    added new view models for course

---
+++ LanguageCeter.Infrastructure/Models/AddCourseVM.cs
@@ -0,0 +1,32 @@
+﻿using LanguageCenter.Core.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanguageCenter.Infrastructure.Models
+{
+    public class AddCourseVM
+    {
+        [Required]
+        [StringLength(Constraints.Course.TitleMaxLength,
+            MinimumLength = Constraints.Course.TitleMinLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(Constraints.Course.LevelLength,
+            MinimumLength = Constraints.Course.LevelLength)]
+        public string Level { get; set; }
+
+        [Range(Constraints.Course.RangeMinValue, Constraints.Course.RangeMaxValue)]
+        public short DurationInMonths { get; set; }
+
+        [StringLength(Constraints.Course.DescriptionLength)]
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [StringLength(Constraints.Language.LanguageNameLength)]
+        public string LanguageName { get; set; }
+
+        public string TeacherId { get; set; }
+    }
+}
---
+++ LanguageCeter.Infrastructure/Models/AllCourseVM.cs
@@ -0,0 +1,15 @@
+﻿namespace LanguageCenter.Infrastructure.Models
+{
+    public class AllCourseVM
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Level { get; set; }
+
+        public string LanguageName { get; set; }
+
+        public string StartDate { get; set; }
+    }
+}
