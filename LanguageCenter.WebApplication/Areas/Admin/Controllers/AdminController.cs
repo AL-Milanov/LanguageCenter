5295025ffddd006ac1ca66d51edfa2cb648f9b78
AL-Milanov <alexandermilanov161195@gmail.com>
2022-04-05 17:28:24 +0300

    renamed home controller in admin area

---
+++ LanguageCenter.WebApplication/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using LanguageCenter.Core.Models.CourseModels;
+using LanguageCenter.Infrastructure.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageCenter.WebApplication.Areas.Admin.Controllers
+{
+    public class AdminController : BaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public AdminController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCourse(AddCourseVM model)
+        {
+            await _courseService.AddAsync(model);
+
+            return View(nameof(Index));
+        }
+    }
+}
