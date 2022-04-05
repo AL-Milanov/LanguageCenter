8a2e5587ccac7e083fb3647e957faf00deb78781
AL-Milanov <alexandermilanov161195@gmail.com>
2022-04-05 17:31:29 +0300

    added course controller with all view

---
+++ LanguageCenter.WebApplication/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using LanguageCenter.Infrastructure.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageCenter.WebApplication.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var courses = await _courseService.GetAllAsync();
+
+            return View(courses);
+        }
+    }
+}
