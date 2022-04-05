4b0d03e3d3d86a3e7d198c7f2709ab4dec563b8e
AL-Milanov <alexandermilanov161195@gmail.com>
2022-04-05 13:43:51 +0300

    created new area

---
+++ LanguageCenter.WebApplication/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using LanguageCenter.Infrastructure.Data.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageCenter.WebApplication.Areas.Admin.Controllers
+{
+    [Authorize(Roles = Constraints.Role.Admin)]
+    [Area(Constraints.Role.Admin)]
+    public class BaseController : Controller
+    {
+    }
+}
