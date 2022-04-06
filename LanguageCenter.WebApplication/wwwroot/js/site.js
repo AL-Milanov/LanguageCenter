02e086e98748b86f7966083d79debee3e061bdd1
AL-Milanov <alexandermilanov161195@gmail.com>
2022-04-06 16:39:59 +0300

    added some js for later

--- LanguageCenter.WebApplication/wwwroot/js/site.js
+++ LanguageCenter.WebApplication/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function onDelete(e) {
+    e.preventDefault();
+    if (!confirm(`Are you sure you want to delete this course`)) {
+        continue;
+    };
+
+    return
+}
