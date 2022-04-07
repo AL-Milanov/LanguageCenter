fc0db8f745649daead21afeeae55f79b780b43a5
AL-Milanov <alexandermilanov161195@gmail.com>
2022-04-07 10:59:38 +0300

    added new controller

--- LanguageCenter.WebApplication/Areas/Admin/Controllers/AdminController.cs
+++ LanguageCenter.WebApplication/Areas/Admin/Controllers/AdminController.cs
@@ -2,21 +2,15 @@
 using LanguageCenter.Core.Services.Contracts;
 using LanguageCenter.Infrastructure.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LanguageCenter.WebApplication.Areas.Admin.Controllers
 {
     public class AdminController : BaseController
     {
-        private readonly ICourseService _courseService;
-        private readonly ILanguageService _languageService;
-
-        public AdminController(
-            ICourseService courseService,
-            ILanguageService languageService)
+        
+        public AdminController()
         {
-            _courseService = courseService;
-            _languageService = languageService;
+
         }
 
         public IActionResult Index()
@@ -24,42 +18,5 @@ namespace LanguageCenter.WebApplication.Areas.Admin.Controllers
             return View();
         }
 
-        public async Task<IActionResult> AddCourse()
-        {
-            var languages = await _languageService.GetAllAsync();
-
-            ViewBag.Languages = languages;
-
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddCourse(AddCourseVM model)
-        {
-            await _courseService.AddAsync(model);
-
-            return View(nameof(Index));
-        }
-
-        public async Task<IActionResult> AllCourses()
-        {
-            var courses = await _courseService.GetAllAsync();
-
-            return View(courses);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> DeleteCourse(string id)
-        {
-            var result = await _courseService.DeleteAsync(id);
-
-            if (!result)
-            {
-                ViewBag.Error = "Something happend course is not deleted.";
-                return View();
-            }
-
-            return RedirectToAction(nameof(AllCourses));
-        }
     }
 }
---
+++ LanguageCenter.WebApplication/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,60 @@
+﻿using LanguageCenter.Core.Models.CourseModels;
+using LanguageCenter.Core.Services.Contracts;
+using LanguageCenter.Infrastructure.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageCenter.WebApplication.Areas.Admin.Controllers
+{
+    public class CourseController : BaseController
+    {
+        private readonly ICourseService _courseService;
+        private readonly ILanguageService _languageService;
+
+        public CourseController(
+            ICourseService courseService,
+            ILanguageService languageService)
+        {
+            _courseService = courseService;
+            _languageService = languageService;
+        }
+
+
+        public async Task<IActionResult> AddCourse()
+        {
+            var languages = await _languageService.GetAllAsync();
+
+            ViewBag.Languages = languages;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCourse(AddCourseVM model)
+        {
+            await _courseService.AddAsync(model);
+
+            return View(nameof(Index));
+        }
+
+        public async Task<IActionResult> AllCourses()
+        {
+            var courses = await _courseService.GetAllAsync();
+
+            return View(courses);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCourse(string id)
+        {
+            var result = await _courseService.DeleteAsync(id);
+
+            if (!result)
+            {
+                ViewBag.Error = "Something happend course is not deleted.";
+                return View();
+            }
+
+            return RedirectToAction(nameof(AllCourses));
+        }
+    }
+}
