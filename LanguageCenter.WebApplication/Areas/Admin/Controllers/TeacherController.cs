65f892452db099bfe68a22050a38f90c0aba3c3c
AL-Milanov <alexandermilanov161195@gmail.com>
2022-04-09 13:52:07 +0300

    teacher contoller

---
+++ LanguageCenter.WebApplication/Areas/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,42 @@
+﻿using LanguageCenter.Core.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LanguageCenter.WebApplication.Areas.Admin.Controllers
+{
+    public class TeacherController : BaseController
+    {
+        private readonly ITeacherService _teacherService;
+        private readonly IUserService _userService;
+        public TeacherController(
+            ITeacherService teacherService,
+            IUserService userService)
+        {
+            _teacherService = teacherService;
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> AllTeachers()
+        {
+            var teachers = await _teacherService.GetAllTeachers();
+
+            return View(teachers);
+        }
+
+        public async Task<IActionResult> AddTeacher()
+        {
+            var users = await _userService
+                .GetAll(u => u.Id != User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var teachers = await _teacherService
+                .GetTeachersId();
+
+
+            var usersNotTeacher = users
+                .Where(u => !teachers.Contains(u.Id))
+                .ToList();
+
+            return View(usersNotTeacher);
+        }
+    }
+}
